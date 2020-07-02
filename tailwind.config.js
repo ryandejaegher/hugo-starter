6f2389da0231c3615516356b7f476192c77ec519
ryandejaegher <dejaegher.ryan@gmail.com>
2020-07-02 12:53:08 -0400

    update

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: {
+    enabled: false,
+    content: ['./themes/my-theme/layouts/**/*.html'],
+  },
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
