4f4b225e11ea42cbb676e01013ecb29d8963fac5
ryandejaegher <dejaegher.ryan@gmail.com>
2020-07-03 12:19:41 -0400

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,14 @@ module.exports = {
     content: ['./themes/my-theme/layouts/**/*.html'],
   },
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: 'var(--primary)',
+        secondary: 'var(--secondary)',
+        'main-text': 'var(--text-main)',
+        'secondary-text': 'var(--text-secondary)',
+      }
+    },
   },
   variants: {},
   plugins: [],
