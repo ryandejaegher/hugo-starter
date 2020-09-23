45df3af728d5ea0d391c8d2caeae31f14a977ac3
ryandejaegher <dejaegher.ryan@gmail.com>
2020-09-23 11:27:26 -0400

    update

--- functions/hello.js
+++ functions/hello.js
@@ -8,32 +8,32 @@ exports.handler = function(event, context, callback) {
 
      const http = require("https");
 
-const options = {
-  "method": "GET",
-  "hostname": "api.netlify.com",
-  "port": null,
-  "path": "/api/v1/sites/ryandejaegher.com/submissions",
-  "headers": {
-    "content-length": "0",
-    "authorization": "Bearer t4scZdWivtlx9SaaUwLQhpAfCAqvlSIQVvhDAtRao9Y"
-  }
-};
-
-const req = http.request(options, function (res) {
-  const chunks = [];
-
-  res.on("data", function (chunk) {
-    chunks.push(chunk);
-  });
-
-  res.on("end", function () {
-    const body = Buffer.concat(chunks);
-    formJSON = body.toString()
-    console.log(body.toString());
-  });
-});
-
-req.end();
+     const options = {
+       "method": "GET",
+       "hostname": "api.netlify.com",
+       "port": null,
+       "path": "/api/v1/sites/ryandejaegher.com/submissions",
+       "headers": {
+         "content-length": "0",
+         "authorization": "Bearer t4scZdWivtlx9SaaUwLQhpAfCAqvlSIQVvhDAtRao9Y"
+       }
+     };
+     
+     const req = http.request(options, function (res) {
+       const chunks = [];
+     
+       res.on("data", function (chunk) {
+         chunks.push(chunk);
+       });
+     
+       res.on("end", function () {
+         const body = Buffer.concat(chunks);
+         formJSON = body
+         console.log(body.toString());
+       });
+     });
+     
+     req.end();
 
     callback(null, {
     statusCode: 200,
