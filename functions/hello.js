51940864b58966b7ad542d0d765a1ccd0e81487a
ryandejaegher <dejaegher.ryan@gmail.com>
2020-09-23 11:15:23 -0400

    update

--- functions/hello.js
+++ functions/hello.js
@@ -3,6 +3,34 @@ exports.handler = function(event, context, callback) {
          name: "ryan",
          age: 31
      };
+
+     const http = require("https");
+
+const options = {
+  "method": "GET",
+  "hostname": "api.netlify.com",
+  "port": null,
+  "path": "/api/v1/sites/ryandejaegher.com/submissions",
+  "headers": {
+    "content-length": "0",
+    "authorization": "Bearer t4scZdWivtlx9SaaUwLQhpAfCAqvlSIQVvhDAtRao9Y"
+  }
+};
+
+const req = http.request(options, function (res) {
+  const chunks = [];
+
+  res.on("data", function (chunk) {
+    chunks.push(chunk);
+  });
+
+  res.on("end", function () {
+    const body = Buffer.concat(chunks);
+    console.log(body.toString());
+  });
+});
+
+req.end();
     callback(null, {
     statusCode: 200,
     body: JSON.stringify(json)
