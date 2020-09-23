8a50acceebbb916cf644237ac1ad36f31ab09c20
ryandejaegher <dejaegher.ryan@gmail.com>
2020-09-23 13:16:07 -0400

    update

--- functions/hello.js
+++ functions/hello.js
@@ -1,42 +1,49 @@
+const http = require("https");
+
+const options = {
+  "method": "GET",
+  "hostname": "api.netlify.com",
+  "port": null,
+  "path": "/api/v1/sites/ryandejaegher.com/submissions",
+  "headers": {
+    "content-length": "0",
+    "authorization": "Bearer t4scZdWivtlx9SaaUwLQhpAfCAqvlSIQVvhDAtRao9Y"
+  }
+};
+
+const req = http.request(options, function (res) {
+  const chunks = [];
+
+  res.on("data", function (chunk) {
+    chunks.push(chunk);
+  });
+
+  res.on("end", function () {
+    const body = Buffer.concat(chunks);
+    console.log(body.toString());
+  });
+});
+
+req.end();
+
 exports.handler = function(event, context, callback) {
-     let json = {
-         name: "ryan",
-         age: 31
-     };
 
-     let formJSON;
-
-     const http = require("https");
-
-     const options = {
-       "method": "GET",
-       "hostname": "api.netlify.com",
-       "port": null,
-       "path": "/api/v1/sites/ryandejaegher.com/submissions",
-       "headers": {
-         "content-length": "0",
-         "authorization": "Bearer t4scZdWivtlx9SaaUwLQhpAfCAqvlSIQVvhDAtRao9Y"
-       }
-     };
-     
-     const req = http.request(options, function (res) {
-       const chunks = [];
-     
-       res.on("data", function (chunk) {
-         chunks.push(chunk);
-       });
-     
-       res.on("end", function () {
-         const body = Buffer.concat(chunks);
-         formJSON = body;   
-         console.log(body.toString());
-       });
-     });
-     
-     req.end();
-
-    callback(null, {    
-    statusCode: 200,
-    body: 'hello'
+    const req = https.request(options, (res) => {
+        let body = '';
+        console.log('Status:', res.statusCode);
+        console.log('Headers:', JSON.stringify(res.headers));
+        res.setEncoding('utf8');
+        res.on('data', (chunk) => body += chunk);
+        res.on('end', () => {
+            console.log('Successfully processed HTTPS response');
+            // If we know it's JSON, parse it
+            if (res.headers['content-type'] === 'application/json') {
+                body = JSON.parse(body);
+            }
+            callback(null, body);
+        });
     });
+    req.on('error', callback);
+    req.write(JSON.stringify(event.data));
+    req.end();
 }
