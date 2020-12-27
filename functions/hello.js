3b4c72304858e016658c2444f26953d4d01649a6
ryandejaegher <dejaegher.ryan@gmail.com>
2020-12-27 11:07:32 -0500

    update

--- functions/hello.js
+++ functions/hello.js
@@ -1,37 +1,17 @@
-const https = require("https");
-
-const options = {
-  "method": "GET",
-  "hostname": "api.netlify.com",
-  "port": null,
-  "path": "/api/v1/sites/ryandejaegher.com/submissions",
-  "headers": {
-    "content-length": "0",
-    "authorization": "Bearer t4scZdWivtlx9SaaUwLQhpAfCAqvlSIQVvhDAtRao9Y"
-  }
-};
+var http = require("http");
 
 
+http.createServer(function (request, response) {
+  // Send the HTTP header 
+  // HTTP Status: 200 : OK
+  // Content Type: text/plain
+  response.writeHead(200, {
+    'Content-Type': 'text/plain'
+  });
 
+  // Send the response body as "Hello World"
+  response.end('Hello World\n');
+}).listen(8081);
 
-exports.handler = function(event, context, callback) {
-
-    const req = https.request(options, (res) => {
-        let body = '';
-        console.log('Status:', res.statusCode);
-        console.log('Headers:', JSON.stringify(res.headers));
-        res.setEncoding('utf8');
-        res.on('data', (chunk) => body += chunk);
-        res.on('end', () => {
-            console.log('Successfully processed HTTPS response');
-            // If we know it's JSON, parse it
-            if (res.headers['content-type'] === 'application/json') {
-                body = JSON.parse(body);
-            }
-            callback(null, body);
-        });
-    });
-    req.on('error', callback);
-    req.write(JSON.stringify(event.data));
-    req.end();
-}
+// Console will print the message
+console.log('Server running at http://127.0.0.1:8081/');
