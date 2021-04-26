3d8743a78fcbe27351a23a2d0deaa340407c32cf
ryandejaegher <dejaegher.ryan@gmail.com>
2021-04-26 11:14:21 -0600

    added algolia purge json file

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,5 @@
 const purgecss = require('@fullhuman/postcss-purgecss')({
-    content: ['./hugo_stats.json'],
+    content: ['./hugo_stats.json','./algolia_css_purge.json'],
     defaultExtractor: content => {
         let els = JSON.parse(content).htmlElements;
         return els.tags.concat(els.classes, els.ids);
